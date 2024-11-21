7e0b2d35e288a545bded1605f695db0809052acd
Ben Baron <bbaron@mparticle.com>
2024-11-21 17:29:30 -0500

    Removed all Xamarin stuff and renamed project to MAUI

--- build.sh
+++ build.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Xamarin
-#
-
-# Restore packages
-nuget restore
-
-# Build bindings
-msbuild Bindings/mParticle.Xamarin.AndroidBinding/mParticle.Xamarin.AndroidBinding.csproj /p:Configuration=Release /t:Rebuild
-msbuild Bindings/mParticle.Xamarin.iOSBinding/mParticle.Xamarin.iOSBinding.csproj /p:Configuration=Release /t:Rebuild
-
-# Build Libraries
-msbuild Library/mParticle.Xamarin.Abstractions/mParticle.Xamarin.Abstractions.csproj /p:Configuration=Release /t:Rebuild
-msbuild Library/mParticle.Xamarin.Android/mParticle.Xamarin.Android.csproj /p:Configuration=Release /t:Rebuild
-msbuild Library/mParticle.Xamarin.iOS/mParticle.Xamarin.iOS.csproj /p:Configuration=Release /t:Rebuild
-
-# Build Sample Apps
-msbuild Samples/mParticle.Xamarin.Android.Sample/mParticle.Xamarin.Android.Sample.csproj /p:Configuration=Debug /t:Rebuild
-msbuild Samples/mParticle.Xamarin.iOS.Sample/mParticle.Xamarin.iOS.Sample.csproj /p:Configuration=Debug /t:Rebuild
-
-
 # .NET MAUI
 # 
 
@@ -43,4 +23,5 @@ dotnet build Samples/mParticle.MAUI.iOS.Sample/mParticle.MAUI.iOS.Sample.csproj 
 # Package for nuget
 #
 
-nuget pack Library/mparticle.nuspec
+# dotnet pack -p:NuspecFile=mparticle.nuspec -p:NuspecBasePath=.
+nuget pack mparticle.nuspec
